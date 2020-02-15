9f6dbe1c6a8e054ddce5597fb0e968b7bbca014b
Shivam Kumar <shivamkmr.50397@gmail.com>
2020-02-15 20:35:15 +0530

    Initial sudoku show up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import generator from "sudoku";
+import SudokuBoard from "./components/SudokuBoard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+  Generate a sudoku with the format
+
+  {rows: [{cols: [{row: 0, col: 0, value: 1, readonly: true}, ...]}, ...]}
+*/
+function genearteSudoku() {
+  const raw = generator.makepuzzle();
+  const result = { rows: [] };
+
+  for (let i = 0; i < 9; i++) {
+    const row = { cols: [], index: i };
+    for (let j = 0; j < 9; j++) {
+      const value = raw[9 * i + j];
+      const col = {
+        row: i,
+        col: j,
+        value,
+        readonly: value !== null
+      };
+      row.cols.push(col);
+    }
+    result.rows.push(row);
+  }
+
+  return result;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sudoku: genearteSudoku()
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>React Sudoku</h1>
+        </header>
+        <SudokuBoard sudoku={this.state.sudoku} />
+      </div>
+    );
+  }
 }
 
 export default App;
