e278669eb7d476c6516b75dafb4a373c17f37d3e
Shivam Kumar <shivamkmr.50397@gmail.com>
2020-02-16 05:33:38 +0530

    Project production build

---
+++ docs/precache-manifest.4677934417992b52d184baa36a0421f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c511d087573e7a48f0d9b5976aae46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c1291ff08a6c188515",
+    "url": "/static/css/main.bec4eeb4.chunk.css"
+  },
+  {
+    "revision": "b799bcb055bf83d4af5b",
+    "url": "/static/js/2.d2938892.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.d2938892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c1291ff08a6c188515",
+    "url": "/static/js/main.794bad5b.chunk.js"
+  },
+  {
+    "revision": "bb62349599070621e598",
+    "url": "/static/js/runtime-main.8484eab2.js"
+  }
+]);
